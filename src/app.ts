44773d4ea8595b6ea4bf26ce99d589174641e05c
Pavlo Rosa <pavlo.rosa@gmail.com>
2020-06-14 21:53:08 +0200

    + create LoggerService

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 // lib/app.ts
 import express from 'express';
+import LoggerService from './config/logger'
+const logger = new LoggerService('./app.ts')
 // Create a new express application instance
 const app: express.Application = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    logger.info('Example app listening on port 3000!');
 });
---
+++ src/config/logger.ts
@@ -0,0 +1,42 @@
+import {createLogger, format, transports} from 'winston';
+import 'colors';
+
+class LoggerService {
+    private logger: any;
+
+    constructor(route: string) {
+        this.logger = createLogger({
+            format: format.combine(
+                format(info => {
+                    info.level = info.level.toUpperCase();
+                    return info;
+                })(),
+                format.colorize(),
+                format.timestamp(),
+                format.align(),
+                format.printf((info) => {
+                    // return `${info.level}: ${info.message}`;
+                    const {timestamp, level, message, ...args} = info;
+
+                    const ts = timestamp.slice(0, 19).replace('T', ' ');
+                    return `${ts} ${route.blue} [${level}]:${message}\n${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+                })
+            ),
+            transports: [new transports.Console()]
+        });
+    }
+
+    async info(message: string) {
+        this.logger.log('info', message);
+    }
+
+    async debug(message: string) {
+        this.logger.log('debug', message);
+    }
+
+    async error(message: string) {
+        this.logger.log('error', message);
+    }
+}
+
+export default LoggerService;
