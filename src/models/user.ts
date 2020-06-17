bc3547dbe5528968b025f0f182a7514a509afddd
Pavlo Rosa <pavlo.rosa@gmail.com>
2020-06-17 20:48:03 +0200

    + eslint + prettier + some refactoring

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,24 +1,28 @@
-import {Sequelize, Model, DataTypes, BuildOptions} from 'sequelize';
+import { Sequelize, Model, DataTypes, BuildOptions } from "sequelize";
 
 export class User extends Model {
-    public id!: number; // Note that the `null assertion` `!` is required in strict mode.
-    public firstName!: string;
-    public lastName!: string;
-    public email!: string;
-    // timestamps!
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
+  public id!: number; // Note that the `null assertion` `!` is required in strict mode.
+  public firstName!: string;
+  public lastName!: string;
+  public email!: string;
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 export default (sequelize: Sequelize) => {
-    User.init({
-        id: {type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true},
-        firstName: {type: DataTypes.STRING},
-        lastName: {type: DataTypes.STRING},
-        email: {type: DataTypes.STRING},
-    }, {
-        tableName: 'Users',
-        sequelize,
-    });
-    return User;
-}
+  User.init(
+    {
+      id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
+      firstName: { type: DataTypes.STRING },
+      lastName: { type: DataTypes.STRING },
+      email: { type: DataTypes.STRING },
+      field1: { type: DataTypes.STRING },
+    },
+    {
+      tableName: "Users",
+      sequelize,
+    }
+  );
+  return User;
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,16 @@
-// var models  = require('../models');
-// import {express} from 'express';
-import {User} from "../models/user";
-import {Request, Response,Router} from 'express';
+import { User } from "../models/user";
+import { Request, Response, Router } from "express";
 
-var router  = Router();
+const router = Router();
 
-router.get('/', function(req: Request, res: Response) {
-   User.findAll({
-        where: {
-            firstName: 'John'
-        }
-    }).then(i =>{res.json(i)});
+router.get("/", function (req: Request, res: Response) {
+  User.findAll({
+    where: {
+      firstName: "John",
+    },
+  }).then((i) => {
+    res.json(i);
+  });
 });
 
 export = router;
-// export {router, ...};
